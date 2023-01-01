15b1c0141c1bd8d50ef41600ce06346831f17724
Rafli Ramadhan <rafliramdhn@gmail.com>
2023-01-01 17:06:01 +0700

    update database

--- cinemasmas.sql
+++ cinemasmas.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2022 at 07:17 AM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 7.3.27
+-- Generation Time: Jan 01, 2023 at 11:05 AM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,51 @@ SET time_zone = "+00:00";
 --
 -- Database: `cinemasmas`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movie`
+--
+
+CREATE TABLE `movie` (
+  `Movie_ID` int(11) NOT NULL,
+  `Title` varchar(50) NOT NULL,
+  `Duration` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `movie`
+--
+
+INSERT INTO `movie` (`Movie_ID`, `Title`, `Duration`) VALUES
+(1, 'Bocchi The Rock!', 2),
+(2, 'SAO', 1),
+(3, 'Chainsawman', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `screening`
+--
+
+CREATE TABLE `screening` (
+  `Screening_ID` int(11) NOT NULL,
+  `TheaterNum` int(11) NOT NULL,
+  `MovieTitle` varchar(50) NOT NULL,
+  `JamTayang` varchar(50) NOT NULL,
+  `Price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `screening`
+--
+
+INSERT INTO `screening` (`Screening_ID`, `TheaterNum`, `MovieTitle`, `JamTayang`, `Price`) VALUES
+(1, 1, 'Bocchi The Rock!', '12:00', 40000),
+(2, 1, 'Chainsaw MId', '14:00', 70000),
+(3, 2, 'Bocchi The Rock!', '12:00', 40000),
+(4, 3, 'Chainsaw MId', '14:00', 70000);
 
 -- --------------------------------------------------------
 
@@ -41,11 +86,23 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`User_ID`, `Name`, `Username`, `Role`, `Password`) VALUES
 (1, 'a', 'a', 'Admin', 'a'),
-(2, 'a', 'v', 'Super Admin', 'v');
+(3, 'Rafli', 'Raflidev', 'Kasir', 'asdasdasd');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `movie`
+--
+ALTER TABLE `movie`
+  ADD PRIMARY KEY (`Movie_ID`);
+
+--
+-- Indexes for table `screening`
+--
+ALTER TABLE `screening`
+  ADD PRIMARY KEY (`Screening_ID`);
 
 --
 -- Indexes for table `user`
@@ -58,10 +115,22 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `movie`
+--
+ALTER TABLE `movie`
+  MODIFY `Movie_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `screening`
+--
+ALTER TABLE `screening`
+  MODIFY `Screening_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `User_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `User_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
